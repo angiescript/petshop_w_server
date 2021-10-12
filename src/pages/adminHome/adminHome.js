e4fd148fe5dd4edb954f7ed60320a2dacd05dff5
angiescript <johansson.angelika@hotmail.com>
2021-10-12 15:16:18 +0200

    Set up boilerplate for project. Delete files.

---
+++ src/pages/adminHome/adminHome.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const adminHome = () => {
+
+    //GET all pets
+    /* 
+      const getPets = async () => {
+        await fetch("http://localhost:5000/customers/") {
+            headers:
+        }
+      }  
+    */
+    return (
+        <div>
+            <h2>Här kommer alla pets att visas upp</h2>
+        </div>
+    )
+}
+
+export default adminHome;
